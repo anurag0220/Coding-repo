d59a06e26223d3168fb72518c54cd66e7913dbe9
Anurag Mishra <91476245+anurag0220@users.noreply.github.com>
2024-01-08 22:17:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarray with given sum/subarray-with-given-sum.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        ArrayList<Integer> li = new ArrayList<Integer>();
+        int curr_sum = 0, start = 0, end = 0;
+        
+        while (end < n) {
+            curr_sum += arr[end];
+            
+            while (curr_sum > s && start < end) {
+                curr_sum -= arr[start];
+                start++;
+            }
+            
+            if (curr_sum == s) {
+                li.add(start + 1);
+                li.add(end + 1);
+                return li;
+            }
+            
+            end++;
+        }
+        
+        li.add(-1);
+        return li;
+    }
+}
